41d9d4a3e82b0ffed40b9fa0e208169476034b3a
Téo Calvo <teocalvo2@gmail.com>
2025-04-21 10:28:15 -0300

    feat: f1 churn score app

---
+++ app/database.py
@@ -0,0 +1,25 @@
+import datetime
+
+from pyspark.sql import SparkSession
+
+import streamlit as st
+
+# %%
+
+@st.cache_resource(ttl=datetime.timedelta(hours=1))
+def connect_spark():
+    spark = (SparkSession.builder
+                         .appName("Streamlit App")
+                         .master("local[2]")
+                         .getOrCreate())
+    return spark
+
+
+@st.cache_data(ttl=datetime.timedelta(hours=6))
+def get_predictions(_spark):
+    return (_spark.read
+                 .format("delta")
+                 .load("s3a://silver/f1/model_churn")
+                 .toPandas())
+    
+    
---
+++ app/main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+import matplotlib.pyplot as plt
+import mplcyberpunk
+plt.style.use("cyberpunk")
+
+from database import connect_spark, get_predictions
+
+
+SPARK = connect_spark()
+data = get_predictions(SPARK)
+
+st.markdown("# F1 Churn Score")
+
+cols = st.columns(3)
+with cols[0]:
+    model = st.selectbox("Modelo de churn", data["nrModelVersion"].unique(), index=0)
+    text = "Confira os detalhes do modelo aqui"
+    model_mlflow = f"http://localhost:5000/#/models/f1_churn/versions/{model}"
+    st.link_button("Detalhes do modelo", model_mlflow, help=text)
+
+with cols[1]:
+    top = st.toggle("Top 10", value=True)
+
+with cols[2]:
+    if top:
+        sorted = data[data["dtRef"]==data["dtRef"].max()].sort_values("pctProbaChurn", ascending=False)
+        n = st.slider("Número de registros", 1, sorted["idDriver"].nunique(), 5)
+        reverse = st.toggle("Reverse", value=False)
+        if reverse:
+            drivers = sorted.tail(n)['idDriver'].unique()
+        else:
+            drivers = sorted.head(n)['idDriver'].unique()
+    else:
+        drivers = st.multiselect("Piloto", data["idDriver"].unique(), default=data["idDriver"].unique())
+
+filters = data["nrModelVersion"] == model
+filters *= data["idDriver"].isin(drivers)
+
+data_show = data[filters]
+st.line_chart(data_show, x="dtRef", y="pctProbaChurn", color="idDriver")
+
+
+st.dataframe(data_show)
